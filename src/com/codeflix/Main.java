4418ff52a062315400e65c5be94d8fc5dd50d32c
GuSimo <17719737+gusimo@users.noreply.github.com>
2021-03-13 20:23:01 +0100

    Aufgabe_05

--- src/com/codeflix/Main.java
+++ src/com/codeflix/Main.java
@@ -6,39 +6,47 @@ public class Main {
 
     public static void main(String[] args) {
 
-        var arr= getRandomArray();
+        int limit = 600;
 
-        // wir müssen speichern, welche Zahlen wir schon gesehen haben.
-        // Dazu wird eine dynamische Liste erzeugt, welche sich ähnlich wie ein Array verhält, man aber die Größe
-        // nicht vorab festlegen muss.
-        ArrayList<Integer> uniqueList = new ArrayList<>();
-
-        // Wir iterieren über das Array
-        for (int item: arr ) {
-            // Wir prüfen, ob unsere Liste den Eintrag des Arrays noch nicht enthält, fügen es dann ein
-            if( ! uniqueList.contains(item)) {
-                uniqueList.add(item);
-            }
-        }
-
-        System.out.println(String.format("Unterschiedliche Einträge: %d", uniqueList.stream().count()));
-
+        Date start = new Date();
+        // System.out.println(fibonacci(limit));
+        System.out.println(fibonacciFast(limit));
+        Date end = new Date();
+        System.out.println((end.getTime() - start.getTime()) / 1000 + " Sekunden");
     }
 
-    public static int[] getRandomArray() {
-        // Wir erzeugen einen Zufallszahlengenerator
-        Random rd = new Random();
-
-        // Wir bereiten unser Array vor
-        int[] arr = new int[10];
-
-        // Wir füllen jedes Feld des Array mit einer Zufallszahl
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = rd.nextInt(20); // hier wird eine Random zahl zwischen 1 und 20 gewählt
-            System.out.println(arr[i]); // Ausgabe zur Überprüfung
+    // Der einfache Ansatz. Nachteil: Die Berechnung wird immer und immer wiederholt, das frisst Zeit.
+    public static long fibonacci(int n) {
+        if (n==1 || n == 2) {
+            return 1;
         }
 
-        return arr;
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+    // Die Hashmap ist wie ein Inhaltsverzeichnis, in diesem Fall findet man zu einem Integer einen passenden Long-Wert
+    private static HashMap<Integer, Long> fibonacciCache = new HashMap<>();
+
+    // Hier werden die vorherigen Ergebnisse zwischengespeichert, dadurch wird CPU Aufwand gegen Speicherverbrauch
+    // getauscht und die Berechnung wird immens schneller.
+    public static long fibonacciFast(int n) {
+
+        if (n==1 || n == 2) {
+            return 1;
+        }
+
+        // Schauen wir in den Cache, ob es schon ein ergebnis gibt
+        if (fibonacciCache.containsKey(n)) {
+            return fibonacciCache.get(n);
+        }
+
+        // Berechnen wir den Wert neu, benutzen dabei jedoch auch unseren Cache.
+        long result = fibonacciFast(n-1) + fibonacciFast(n-2);
+
+        // Neue Ergebnisse legen wir in den Cache
+        fibonacciCache.put(n, result);
+
+        return result;
     }
 
 }
