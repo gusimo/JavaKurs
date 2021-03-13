73c9fbafc1ee6c09e0b7209265933dc205c9f317
GuSimo <17719737+gusimo@users.noreply.github.com>
2021-03-13 20:06:02 +0100

    Aufgabe 02

--- src/com/codeflix/Main.java
+++ src/com/codeflix/Main.java
@@ -1,82 +1,44 @@
 package com.codeflix;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    private static Scanner input;
-
     public static void main(String[] args) {
-        // Zuerst holen wir alle Variablen vom Anwender ab
-        // Dazu wird die Hilfsklasse Scanner benötigt. Details zu Klassen folgen später, sie könne hier wie ein
-        // eigener Variablen-Typ angesehen werden.
 
-        input = new Scanner(System.in);
+        var arr= getRandomArray();
 
-        int quotient = getQuotient();
-        int counter = getCounter();
-        boolean descending = getDescending();
+        // wir müssen speichern, welche Zahlen wir schon gesehen haben.
+        // Dazu wird eine dynamische Liste erzeugt, welche sich ähnlich wie ein Array verhält, man aber die Größe
+        // nicht vorab festlegen muss.
+        ArrayList<Integer> uniqueList = new ArrayList<>();
 
-        if (descending) {
-            for (int i = counter; i > 0; i--) {
-                if (i % quotient == 0) {
-                    System.out.println(i);
-                }
-            }
-        } else {
-            for (int i = 1; i <= counter; i++) {
-                if (i % quotient == 0) {
-                    System.out.println(i);
-                }
+        // Wir iterieren über das Array
+        for (int item: arr ) {
+            // Wir prüfen, ob unsere Liste den Eintrag des Arrays noch nicht enthält, fügen es dann ein
+            if( ! uniqueList.contains(item)) {
+                uniqueList.add(item);
             }
         }
 
+        System.out.println(String.format("Unterschiedliche Einträge: %d", uniqueList.stream().count()));
+
     }
 
-    public static int getQuotient() {
-        System.out.print("Wie ist der Teiler? ");
-        int quotient = input.nextInt();
+    public static int[] getRandomArray() {
+        // Wir erzeugen einen Zufallszahlengenerator
+        Random rd = new Random();
 
-        // Prüfung der Eingabe und abbruch des Programms bei Fehlern
-        if (quotient < 1 || quotient > 99) {
-            programError("Der Teiler darf nur zwischen 1 und 99 liegen.");
+        // Wir bereiten unser Array vor
+        int[] arr = new int[10];
+
+        // Wir füllen jedes Feld des Array mit einer Zufallszahl
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rd.nextInt(20); // hier wird eine Random zahl zwischen 1 und 20 gewählt
+            System.out.println(arr[i]); // Ausgabe zur Überprüfung
         }
 
-        return quotient;
+        return arr;
     }
 
-    public static int getCounter() {
-        System.out.print("Bis wohin wird gezählt? ");
-        int counter = input.nextInt();
-
-        if (counter < 1) {
-            programError("Der Teiler darf nur zwischen 1 und 99 liegen.");
-        }
-
-        return counter;
-    }
-
-    public static boolean getDescending() {
-        System.out.print("Soll Rückwärts ausgegeben werden? (N/j)");
-        String decision = input.next();
-
-        // Gib den Standardwert aus, wenn der User nichts eingibt oder N
-        if (decision.length() == 0 || decision.equalsIgnoreCase("n")) {
-            return false;
-        }
-
-        if (decision.equalsIgnoreCase("j")) {
-            return true;
-        }
-
-        // Wenn es weder ja noch nein ist, werfen wir einen Fehler
-        programError("Geben Sie j oder n ein.");
-
-        return false;
-    }
-
-    public static void programError(String message) {
-        System.out.println("FEHLER:" + message);
-        System.exit(1);
-    }
 }
