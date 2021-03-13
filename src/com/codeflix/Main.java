8dfb7092e82f0af89ced3a2c8447ddbacbd645d5
GuSimo <17719737+gusimo@users.noreply.github.com>
2021-03-13 20:39:22 +0100

    Aufgabe_04

--- src/com/codeflix/Main.java
+++ src/com/codeflix/Main.java
@@ -6,48 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int limit = 600;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 4 == 0 || i % 7 == 0) {
+                if (i % 4 == 0) {
+                    System.out.print("Fizz");
+                }
+                if (i % 7 == 0) {
+                    System.out.print("Buzz");
+                }
 
-        Date start = new Date();
-        System.out.print(String.format("f(%d) = ", limit));
-        // System.out.println(fibonacci(limit));
-        System.out.println(fibonacciFast(limit));
-        Date end = new Date();
-        System.out.println((end.getTime() - start.getTime()) / 1000 + " Sekunden");
-    }
-
-    // Der einfache Ansatz. Nachteil: Die Berechnung wird immer und immer wiederholt, das frisst Zeit.
-    public static long fibonacci(int n) {
-        if (n==1 || n == 2) {
-            return 1;
+                System.out.println("");
+            } else {
+                System.out.println(i);
+            }
         }
-
-        return fibonacci(n-1) + fibonacci(n-2);
     }
-
-    // Die Hashmap ist wie ein Inhaltsverzeichnis, in diesem Fall findet man zu einem Integer einen passenden Long-Wert
-    private static HashMap<Integer, Long> fibonacciCache = new HashMap<>();
-
-    // Hier werden die vorherigen Ergebnisse zwischengespeichert, dadurch wird CPU Aufwand gegen Speicherverbrauch
-    // getauscht und die Berechnung wird immens schneller.
-    public static long fibonacciFast(int n) {
-
-        if (n==1 || n == 2) {
-            return 1;
-        }
-
-        // Schauen wir in den Cache, ob es schon ein ergebnis gibt
-        if (fibonacciCache.containsKey(n)) {
-            return fibonacciCache.get(n);
-        }
-
-        // Berechnen wir den Wert neu, benutzen dabei jedoch auch unseren Cache.
-        long result = fibonacciFast(n-1) + fibonacciFast(n-2);
-
-        // Neue Ergebnisse legen wir in den Cache
-        fibonacciCache.put(n, result);
-
-        return result;
-    }
-
 }
