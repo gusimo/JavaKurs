23ad474ed4afdda1c27931b502e6e40f201313c9
GuSimo <17719737+gusimo@users.noreply.github.com>
2021-03-14 09:38:56 +0100

    Aufgabe 3

--- src/com/codeflix/Main.java
+++ src/com/codeflix/Main.java
@@ -6,25 +6,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> lotto = new ArrayList<>();
-        Random random = new Random();
+        // Zuerst legen wir die Variablen fest, sie können natürlich auch vom Benutzer abgefragt werden
+        int age = 56;
+        double income = 2000.00;
+        boolean house = true;
+        String gender = "Male";
 
-        while (lotto.stream().count() < 6) {
-            int ball = random.nextInt(49);
+        // Wir schließen zuers die Altersgrenzen aus
+        if ( age < 18 || ( house && age > 70 ) || ( !house && age > 55 ) ) {
+            System.out.println("Das Alter passt nicht");
+            return;
+        }
 
-            if (!lotto.contains(ball)) {
-                lotto.add(ball);
+        // Dann bestimmen wir das Alter, bis zu dem gerechnet werden darf
+        int maximumAge = 55;
+        if (house) {
+            if (gender.equalsIgnoreCase("male")) {
+                maximumAge = 70;
+            }
+            if (gender.equalsIgnoreCase("female")) {
+                maximumAge = 74;
             }
         }
 
-        Collections.sort(lotto);
-
-        for (int i=0; i < lotto.stream().count(); i++) {
-            if (i >0) {
-                System.out.print(", ");
-            }
-
-            System.out.print(lotto.get(i));
-        }
+        // Jetzt bestimmen wir den Kreditbetrag
+        int maximumMonths = (maximumAge - age) * 12;
+        double maxCredit = maximumMonths * income;
+        
+        System.out.println(String.format("%.2f Euro", maxCredit));
     }
 }
