31a7a17f8b915e096a627c80bcec2f365b2fc478
GuSimo <17719737+gusimo@users.noreply.github.com>
2021-03-27 20:14:44 +0100

    added list item test

--- src/main/java/com/codeflix/selenium/SmokeTest.java
+++ src/main/java/com/codeflix/selenium/SmokeTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -32,10 +33,40 @@ public class SmokeTest {
     }
 
     @Test
-    public void seleniumTest() {
+    public void checkHeader() {
+        browser.get("https://hiq.de");
+
+        String[] expected = new String[7];
+        expected[0] = "Über HiQ";
+        expected[1] = "Leistungen";
+        expected[2] = "Unsere Kunden";
+        expected[3] = "Karriere";
+        expected[4] = "News & Presse";
+        expected[5] = "Webinare & Events";
+        expected[6] = "Kontakt";
+
+        List<WebElement> items = browser.findElements(By.className("menu-item"));
+
+        for (String expectedItem:expected) {
+            boolean foundItem = false;
+            for(WebElement existingItem: items) {
+                if (existingItem.getText().equals(expectedItem)) {
+                    foundItem = true;
+                }
+            }
+            assertTrue(foundItem, expectedItem);
+        }
+    }
+
+    @Test
+    public void checkMenu() {
         browser.get("https://hiq.de");
         WebElement header = browser.findElement(By.id("header"));
         assertTrue((header.isDisplayed()));
+    }
+
+    @AfterAll
+    public static void closeBrowser() {
         browser.close();
     }
 
