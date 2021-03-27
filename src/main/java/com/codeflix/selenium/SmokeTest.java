bb879a4865eccb66c1bd95dd07a0b85af9ed83c8
GuSimo <17719737+gusimo@users.noreply.github.com>
2021-03-27 13:40:25 +0100

    Firefox Demo hinzugefügt

--- src/main/java/com/codeflix/selenium/SmokeTest.java
+++ src/main/java/com/codeflix/selenium/SmokeTest.java
@@ -1,12 +1,25 @@
 package com.codeflix.selenium;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SmokeTest {
 
     @Test
-    public void seleniumChromeTest() {
-
+    public void seleniumFirefoxTest() {
+        WebDriver browser;
+        //Firefox's geckodriver *requires* you to specify its location.
+        System.setProperty("webdriver.gecko.driver", "/etc/selenium/geckodriver");
+        browser = new FirefoxDriver();
+        browser.get("https://hiq.de");
+        WebElement header = browser.findElement(By.id("header"));
+        assertTrue((header.isDisplayed()));
+        browser.close();
     }
 
 }
